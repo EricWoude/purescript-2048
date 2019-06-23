de6da6bd8d017a4b7900eaa377eff305826ec53c
Eric van der Woude <ericvdwoude@gmail.com>
2019-06-23 12:04:01 +0200

    Initial project files

---
+++ README.md
@@ -0,0 +1,5 @@
+# 2048 in Purescript
+This project aims to implement the famous game [2048](https://en.wikipedia.org/wiki/2048_(video_game)) in purescript. It may (~~will~~) contain design errors and other quirks, as this is just made to increase my functional programming skills.
+
+## Acknowledgments
+My work is highly inspired by the blog post [Implementing the game 2048 in less than 90 lines of Haskell](http://gregorulm.com/2048-in-90-lines-haskell/) by Gregor Ulm.
---
+++ src/Main.purs
@@ -0,0 +1,38 @@
+-- | Implements the game 2048 using purescript. It is highly inspired by
+-- | the blog post "Implementing the game 2048 in less than 90 lines of Haskell"
+-- | by Gregor Ulm (http://gregorulm.com/2048-in-90-lines-haskell/)
+
+module Main where
+
+import Prelude
+import Data.List.Types (List(..), (:))
+import Data.List (filter, length, concat)
+import Data.Unfoldable (replicate)
+
+data Move = Up | Down | Left | Right
+type Grid = List (List Int)
+
+grid :: Grid
+grid = ((0 : 0 : 0 : 0 : Nil) :
+        (0 : 0 : 0 : 0 : Nil) :
+        (0 : 0 : 0 : 0 : Nil) :
+        (0 : 0 : 2 : 2 : Nil) : Nil)
+
+
+-- | Takes a list, removes the zeros, adds duplicate neighbouring numbers and adds padding.
+merge :: List Int -> List Int
+merge xs = concat (combined : padding : Nil)
+  where padding = replicate (length xs - length combined) 0
+        combined = combine $ filter (\n -> n > 0) xs
+
+-- | Takes a list, combines two duplicate neighbouring elements in a list and returns list.
+combine :: List Int -> List Int
+combine (x:y:xs) = if x == y then x * 2 : combine (xs) else x : combine (y:xs)
+combine x        = x
+
+-- | Given the Grid and Move, do merge for all rows in the Grid.
+move :: Grid -> Move -> Grid
+move grid Left  = map merge grid -- TODO: different ruling for different moves
+move grid Up    = map merge grid
+move grid Down  = map merge grid
+move grid Right = map merge grid
