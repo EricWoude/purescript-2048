19058951306a93b014b5e29c733a20681bae3425
Eric van der Woude <ericvdwoude@gmail.com>
2019-07-02 16:59:08 +0200

    Keyboard event listeners added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ import {main} from '../output/Main/index'
 
 const app = document.getElementById('app')
 
+document.onkeydown = keyControl;
+
 /* Takes the grid as HTML string and appends it to the app. */
 const drawGrid = () => {
     app.insertAdjacentHTML('beforeend', main)
 }
 
+function keyControl(event) {
+    switch(event.keyCode) {
+        case 37: // left arrow
+            break
+
+        case 38: // up arrow
+            break
+    
+        case 39: // right arrow
+            break
+
+        case 40: // down arrow
+            break
+    }
+}
+
 drawGrid()
+
