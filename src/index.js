709a4cf79a5c008d2ef34fb973ccf09a5e71acf4
Eric van der Woude <ericvdwoude@gmail.com>
2019-07-02 14:57:24 +0200

    Added index.js/index.html to showcase 2048 in

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Main from '../output/Main/index'
+
+console.log(Main.main())
+// function makeTable(array) {1
+//     var table = document.createElement('table');
+//     for (var i = 0; i < array.length; i++) {
+//         var row = document.createElement('tr');
+//         for (var j = 0; j < array[i].length; j++) {
+//             var cell = document.createElement('td');
+//             cell.textContent = array[i][j];
+//             row.appendChild(cell);
+//         }
+//         table.appendChild(row);
+//     }
+//     return table;
+// }
